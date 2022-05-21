0f3d952b2e618378b6aa1c423585ab221b50c8fe
SvennyBOI <psven595@gmail.com>
2022-05-22 00:04:09 +0200

    Info site

--- src/Components/TypingCard.js
+++ src/Components/TypingCard.js
@@ -8,6 +8,7 @@ import {
   Text,
   Divider,
   Row,
+  User,
 } from "@nextui-org/react";
 import { useState, useRef } from "react";
 
@@ -15,18 +16,17 @@ const getWords = () =>
   `the at there some my of be use her than and this an would first a have each make water to from which like been in or she him call is one do into who you had how time oil that by their has its it word if look now he but will two find was not up more long for what other write down on all about go day are were out see did as we many number get with when then no come his your them way made they can these could may I said so people 
 part`.split(" ");
 
-
 //.sort(() => Math.random() > 0.5 ? 1 :-1)
 function TypingCard({}) {
   const [userInput, setUserInput] = useState("");
   const word = useRef(getWords());
-  const [progress, setProgress] = useState(0); 
+  const [progress, setProgress] = useState(0);
 
   const [activeWordIndex, setActiveWordIndex] = useState(0);
   function processInput(value) {
     if (value.endsWith(" ")) {
       setActiveWordIndex((index) => index + 1);
-      setProgress((progress) => progress+1)
+      setProgress((progress) => progress + 1);
       setUserInput("");
     } else {
       setUserInput(value);
@@ -41,7 +41,27 @@ function TypingCard({}) {
           backdropFilter: "blur(1px)",
         }}
       >
-        <Progress color="secondary" value={progress}></Progress>
+        <div className="flex">
+          <User
+            src="https://avatars.githubusercontent.com/u/97550617?v=4"
+            name="Sven"
+            bordered
+            color="secondary"
+          ></User>
+          <Progress color="secondary" value={progress}></Progress>
+          <p className="m-4 font-bold">{progress}%</p>
+        </div>
+        <div className="m-4"></div>
+        <div className="flex">
+          <User
+            src="https://qph.cf2.quoracdn.net/main-qimg-ab45488a5f2a231287ab232486154ae0-lq"
+            name="User1"
+            bordered
+            color="error"
+          ></User>
+          <Progress color="error" value={75}></Progress>
+          <p className="m-4 font-bold">{75}%</p>
+        </div>
         <Divider className="my-4"></Divider>
         <Text>{getWords}</Text>
         <Text css={{ fontFamily: "monospace", fontSize: "$md" }}>
@@ -56,11 +76,16 @@ function TypingCard({}) {
             return <span>{word} </span>;
           })}
         </Text>
-        <Input
+        <Input 
+          size="xl"
+          autocomplete="off"
+          autocorrect="off"
+          autocapitalize="off"
+          spellcheck="false"
+          color="secondary"
           clearable
           underlined
           initialValue="NextUI"
-          color="secondary"
           value={userInput}
           onChange={(e) => processInput(e.target.value)}
         />
--- src/SubPages/Info.js
+++ src/SubPages/Info.js
@@ -10,6 +10,7 @@ import {
   Divider,
   Grid,
   Table,
+  Image,
 } from "@nextui-org/react";
 function Info() {
   return (
@@ -25,92 +26,105 @@ function Info() {
       >
         Gryd - Info
       </Text>
-
-        <Text h2 className="headspace">
-          About
-        </Text>
-        <Text h3 className="pspace">
-          Gryd is{" "}
-          <span className=" bg-opacity-40 bg-violet-800 ">
-            modern typing test
-          </span>
-          , featuring many test modes, an account system to save your typing
-          speed history and user configurable features like themes, a smooth
-          caret and more. Gryd is an{" "}
-          <span className=" bg-opacity-40 bg-violet-800 ">open source</span>{" "}
-          project, so you can go checkout the{" "}
-          <a className=" font-semibold">Github </a>repository. Gryd uses the{" "}
-          <span className="bg-opacity-40 bg-violet-800">
-            most common 100 words
-          </span>{" "}
-          in the English language to generate its tests. You can use{" "}
-          <span className="bg-opacity-40 bg-violet-800"> tab to restart</span>{" "}
-          the typing test while typing or at the result of the test. After
-          completing a test you will be able to see your wpm.{" "}
-          <span className="bg-opacity-40 bg-violet-800">
-            {" "}
-            Wpm stands for words per minute
-          </span>{" "}
-        </Text>
-        <Text h2 className="headspace">
-          Bug report or feature request
-        </Text>
-        <Text h3>
-          If you encounter a bug, or have a feature request - join the Discord
-          server, send me an email, a direct message on Twitter or create an
-          issue on GitHub. Or, send me a message through this box:
-        </Text>
-        <Text h3 className="pspace">
+      <Text h2 className="headspace">
+        About
+      </Text>
+      <Text h3 className="pspace">
+        Gryd is{" "}
+        <span className=" bg-opacity-40 bg-violet-800 ">
+          modern typing test
+        </span>
+        , featuring many test modes, an account system to save your typing speed
+        history and user configurable features like themes, a smooth caret and
+        more. Gryd is an{" "}
+        <span className=" bg-opacity-40 bg-violet-800 ">open source</span>{" "}
+        project, so you can go checkout the{" "}
+        <a className=" font-semibold">Github </a>repository. Gryd uses the{" "}
+        <span className="bg-opacity-40 bg-violet-800">
+          most common 100 words
+        </span>{" "}
+        in the English language to generate its tests. You can use{" "}
+        <span className="bg-opacity-40 bg-violet-800"> tab to restart</span> the
+        typing test while typing or at the result of the test. After completing
+        a test you will be able to see your wpm.{" "}
+        <span className="bg-opacity-40 bg-violet-800">
           {" "}
-          Or, send me a message through this box:
-        </Text>
-        <div className="flex gap-3">
-          <Textarea
-            width="800px"
-            labelPlaceholder="Write down your thoughts"
-            status="secondary"
-            className="pspace"
-          />
-          <Tooltip content={"You can only send a message once, so be careful"}>
-            <Button
-              auto
-              color="secondary"
-              flat
-              className="bg-violet-800 bg-opacity-20"
-            >
-              Send
-            </Button>
-          </Tooltip>
-        </div>
-        <Text h2 className="headspace">
-          Changelog
-        </Text>
-        <a>Current Version: 1.0</a>
-        <Text h3 className="headspace"></Text>
-        {/* --- */}
-        <ol>
+          Wpm stands for words per minute
+        </span>{" "}
+      </Text>
+      <Text h2 className="headspace">
+        Bug report or feature request
+      </Text>
+      <Text h3>
+        If you encounter a bug, or have a feature request - join the Discord
+        server, send me an email, a direct message on Twitter or create an issue
+        on GitHub. Or, send me a message through this box:
+      </Text>
+      <Text h3 className="pspace">
+        {" "}
+        Or, send me a message through this box:
+      </Text>
+      <div className="flex gap-3">
+        <Textarea
+          width="800px"
+          labelPlaceholder="Write down your thoughts"
+          status="default"
+          className="pspace"
+        />
+        <Tooltip content={"You can only send a message once, so be careful"}>
+          <Button
+            auto
+            color="secondary"
+            flat
+            className="bg-violet-800 bg-opacity-20"
+          >
+            Send
+          </Button>
+        </Tooltip>
+      </div>
+      <Text h2 className="headspace">
+        Changelog
+      </Text>
+      <a>Current Version: 1.0</a>
+      <Text h3 className="headspace"></Text>
+      {/* --- */}
+      <ol className="pspace">
         <li>- Added new background</li>
         <li>- Added Glassmorphism</li>
         <li>- Added Settings/Info/Home page</li>
         <li>- Added Prograss bar on top of test</li>
+      </ol>
 
-        </ol>
-
-        {/* --- */}
-        <Text h2 className="headspace">Contact</Text>
+      {/* --- */}
+      <Text h2 className="headspace">
+        Contact
+      </Text>
+      <Grid justify="center" align="center">
         <User
-      src="https://avatars.githubusercontent.com/u/97550617?v=4"
-      name="ThinkingSven - Dev"
-      color="secondary"
-      bordered
-    />
-    <Grid>
-
-         <Button flat color="primary" auto>
-          Secondary
-        </Button>
-     
-    </Grid>
+          src="https://avatars.githubusercontent.com/u/97550617?v=4"
+          name="ThinkingSven - Dev"
+          color="secondary"
+          bordered
+          className="headspace"
+        />
+      </Grid>
+      <Grid.Container gap={4} justify="center" align="center">
+        <Grid>
+          <Button size="lg" className="bg-blue-500 " color="primary">
+            Twitter
+          </Button>
+        </Grid>
+        <Grid>
+          <Button size="lg" className="bg-gray-500 " color="default">
+            Discord
+          </Button>
+        </Grid>
+        <Grid>
+          <Button size="lg" className="bg-violet-500 " color="secondary">
+            Email
+          </Button>
+        </Grid>
+      </Grid.Container>
     </div>
   );
 }
