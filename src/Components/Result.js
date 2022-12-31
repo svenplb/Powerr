58d3dcab1a29061e5a7e7402850a6cbc14d3a15e
SvennyBOI <psven595@gmail.com>
2022-12-31 14:56:47 +0100

    Update design - added background colors to CSS

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -3,7 +3,7 @@ const Result = (props) => {
     const { correctWords, startCounting, totalWords, timeElapsed, testFinished, setTestFinished} = props;
     const minutes = timeElapsed / 60;
     return(
-        <div className=" bg-neutral-900 p-20 rounded-xl shadow-xl shadow-zinc-900">
+        <div className="">
             <p>Speed: {Math.round(correctWords / minutes || 0)} WPM</p>
             <p>acc: {100 - ((totalWords.length - correctWords)/(totalWords.length/100))}%</p>
             <p>raw wpm: {Math.round(totalWords.length / minutes || 0)}</p>
