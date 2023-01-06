6e3a0fd3752ce1e5e48ff4fa309fa1df4e76f1f3
ThinkingSven <psven595@gmail.com>
2023-01-06 12:13:15 +0100

    added firebase & google sign in to the login button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import AppRoutes from "./Components/AppRoutes";
 
-function App() {
-  // TODO Make a AppRoutes Component
-  // TODO Handle the theme & wordamount in a different way
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAjHbhBZbH-odqYn0fXuRHFpQdD2t8X_X0",
+  authDomain: "powerr-6a858.firebaseapp.com",
+  projectId: "powerr-6a858",
+  storageBucket: "powerr-6a858.appspot.com",
+  messagingSenderId: "314207896981",
+  appId: "1:314207896981:web:c32437f0b3b1b9b594229c",
+  measurementId: "G-MCMK1F6MVJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {
   return (
     <div>
       <AppRoutes />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,4 @@
+import { Login } from "./Login";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -20,9 +21,7 @@ function Navbar() {
           </div>
           {/* Darkmode test */}
           <div className="flex">
-            <button>
-              hey
-            </button>
+            <Login />
           </div>
         </div>
       </div>
