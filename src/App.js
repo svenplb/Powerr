a44a7e924197d24eaf71dd228c273644b1a7b906
ThinkingSven <psven595@gmail.com>
2023-01-07 17:02:54 +0100

    Resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppRoutes from "./Components/AppRoutes";
+
 function App() {
   // TODO Make a AppRoutes Component
   // TODO Handle the theme & wordamount in a different way
